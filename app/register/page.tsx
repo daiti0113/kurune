e00920c18adf2067fe3bdba3a83bd928d7fb4172
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-02 17:24:49 +0900

    フォームのサンプルコードを追加

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import { Button } from "@/components/atoms/Button";
+import { FormRoot } from "@/components/atoms/Organisms/Form/FormRoot";
+import { FormControl, FormField, FormLabel, FormMessage, FormSubmit } from "@radix-ui/react-form";
 
 export default function Register() {
     return (
@@ -6,6 +10,21 @@ export default function Register() {
             <h1>Register</h1>
             <Button>これはプライマリーボタンです</Button>
             <Button variant="secondary">これはセカンダリーボタンです</Button>
+            <FormRoot>
+                <FormField name="email">
+                    <FormLabel>メールアドレス</FormLabel>
+                    <FormMessage className="FormMessage" match="valueMissing">
+                        メールアドレスを入力してください
+                    </FormMessage>
+                    <FormMessage match="typeMismatch">メールアドレスの形式が誤っています</FormMessage>
+                    <FormControl asChild>
+                        <input type="email" required />
+                    </FormControl>
+                </FormField>
+                <FormSubmit asChild>
+                    <Button>出品する</Button>
+                </FormSubmit>
+            </FormRoot>
         </div>
     )
 }
