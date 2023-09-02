e9c35bf6b7086266fa3dbbca7076e1cbfa30fbc4
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-02 18:14:16 +0900

    TextInputのスタイルを追加

--- components/atoms/TextInput/index.tsx
+++ components/atoms/TextInput/index.tsx
@@ -4,6 +4,6 @@ type TextInputProps = InputHTMLAttributes<HTMLInputElement>
 
 export const TextInput: React.FC<TextInputProps> = (props) => {
     return (
-        <input {...props} />
+        <input className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" {...props} />
     )
 }
