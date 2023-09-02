18fa99d4492573488da6ae55c0167656c396a906
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-02 18:51:22 +0900

    商品情報入力フォームを追加

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,26 +1,60 @@
 "use client"
 
 import { Button } from "@/components/atoms/Button";
+import { ImageInput } from "@/components/atoms/ImageInput";
+import { TextArea } from "@/components/atoms/TextArea";
 import { TextInput } from "@/components/atoms/TextInput";
 import Form from "@/components/organisms/Form";
 
 export default function Register() {
     return (
         <div>
-            <h1>Register</h1>
-            <Button>これはプライマリーボタンです</Button>
-            <Button variant="secondary">これはセカンダリーボタンです</Button>
+            <h1 className="text-xl font-bold">出品する</h1>
             <Form>
-                <Form.Field name="email">
-                    <Form.Label>メールアドレス</Form.Label>
-                    <Form.Message className="FormMessage" match="valueMissing">
-                        メールアドレスを入力してください
-                    </Form.Message>
-                    <Form.Message match="typeMismatch">メールアドレスの形式が誤っています</Form.Message>
-                    <Form.Control asChild>
-                        <TextInput type="email" required />
-                    </Form.Control>
-                </Form.Field>
+                <div className="mt-4 flex flex-col gap-10">
+                    <Form.Field name="image">
+                        <Form.Label>商品画像</Form.Label>
+                        <Form.Control asChild>
+                            <ImageInput required />
+                        </Form.Control>
+                        <Form.Message match="valueMissing">
+                            画像を添付してください
+                        </Form.Message>
+                    </Form.Field>
+                    <Form.Field name="title">
+                        <Form.Label>タイトル</Form.Label>
+                        <Form.Control asChild>
+                            <TextInput required />
+                        </Form.Control>
+                        <Form.Message match="valueMissing">
+                            タイトルを入力してください
+                        </Form.Message>
+                    </Form.Field>
+                    <Form.Field name="description">
+                        <Form.Label>説明</Form.Label>
+                        <Form.Control asChild>
+                            <TextArea required />
+                        </Form.Control>
+                        <Form.Message match="valueMissing">
+                            説明を入力してください
+                        </Form.Message>
+                    </Form.Field>
+                </div>
+                <div className="mt-20">
+                    <h2 className="text-lg font-bold">出品者情報</h2>
+                    <div className="mt-4 flex flex-col gap-10">
+                        <Form.Field name="email">
+                            <Form.Label>メールアドレス</Form.Label>
+                            <Form.Control asChild>
+                                <TextInput type="email" required />
+                            </Form.Control>
+                            <Form.Message match="valueMissing">
+                                メールアドレスを入力してください
+                            </Form.Message>
+                            <Form.Message match="typeMismatch">メールアドレスの形式が誤っています</Form.Message>
+                        </Form.Field>
+                    </div>
+                </div>
                 <Form.Submit asChild>
                     <Button>出品する</Button>
                 </Form.Submit>
