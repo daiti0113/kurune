b2f1acb47b5b451936f72324401751dcb624cf80
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-17 13:49:35 +0900

    とりあえずFileInputを仮でレスポンシブ対応

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,29 +1,12 @@
 "use client"
 
 import { useRef, useState } from "react";
-import { useDropzone } from "react-dropzone";
 import { useUpload } from "@/hooks/s3";
+import { FileInput } from "@/components/molecules/FileInput";
 
 function UploadImage() {
-  const inputRef = useRef<HTMLInputElement>(null);
   const [url, setUrl] = useState("");
   const { upload, isLoading } = useUpload()
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop: upload,
-  });
-
-  const fileUpload = () => {
-    if (inputRef.current == null) return;
-    inputRef.current.click();
-  };
-
-  const onFileInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files == null) return;
-    console.log(event.target.files[0]);
-    const fileUrl = await upload([event.target.files[0]])
-    console.log({fileUrl})
-    fileUrl && setUrl(fileUrl)
-  };
 
   return (
     <div className="App">
@@ -32,51 +15,7 @@ function UploadImage() {
         {isLoading ? (
           <h1>アップロード中・・・</h1>
         ) : (
-          <div
-            className="rounded shadow-lg w-1/2"
-            style={{ maxWidth: "400px", minWidth: "300px" }}
-          >
-            <div className="flex justify-center">
-              <div className="my-8 flex justify-center grid">
-                <h1 className="font-bold text-gray-500 col-span-3 text-center">
-                  動画アップロード
-                </h1>
-                <div className="text-gray-500 col-span-3 text-center mt-2 mb-4 text-xs">
-                  MP4の動画ファイルを選択
-                </div>
-                <div
-                  className="border-dashed border-2 border-gray-500 grid flex justify-cente p-4 mb-2 cursor-pointer"
-                  style={{ minWidth: "200px" }}
-                  {...getRootProps()}
-                >
-                  <input {...getInputProps()} />
-                  <div className="container flex justify-center mb-2">
-                  </div>
-                  <div className="container flex justify-center text-xs text-gray-500">
-                    <p>ここにドラッグ＆ドロップしてね</p>
-                  </div>
-                </div>
-                <div className="col-span-3 text-center my-1 text-gray-500">
-                  または
-                </div>
-                <div className="col-span-3 flex justify-center w-full">
-                  <button
-                    onClick={fileUpload}
-                    className="bg-blue-500 rounded text-white font-bold py-2 px-4 w-full"
-                  >
-                    ファイルを選択
-                  </button>
-                  <input
-                    type="file"
-                    className="hidden"
-                    accept=".jpg,.jpeg,.png"
-                    ref={inputRef}
-                    onChange={onFileInputChange}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+            <FileInput onDrop={upload} />
         )}
       </div>
     </div>
---
+++ components/molecules/FileInput/index.tsx
@@ -0,0 +1,61 @@
+import { useWindowSize } from "@/hooks/window";
+import { useRef, useState } from "react";
+import { DropEvent, FileRejection, useDropzone } from "react-dropzone";
+
+type FileInputProps = {
+    onDrop: <T extends File>(acceptedFiles: T[], fileRejections?: FileRejection[], event?: DropEvent) => void
+}
+
+export const FileInput: React.FC<FileInputProps> = ({onDrop}) => {
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop,
+    });
+    const { width } = useWindowSize()
+    const inputRef = useRef<HTMLInputElement>(null);
+  const [url, setUrl] = useState("");
+
+  const fileUpload = () => {
+    if (inputRef.current == null) return;
+    inputRef.current.click();
+  };
+
+  const onFileInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files == null) return;
+    console.log(event.target.files[0]);
+    onDrop([event.target.files[0]])
+  };
+
+    if (width > 960) {
+        return (
+            <div className="flex items-center justify-center w-full">
+                <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600" {...getRootProps()}>
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                    </div>
+                    <input {...getInputProps()} />
+                </label>
+            </div>
+          )
+    }
+    return (
+        <div className="col-span-3 flex justify-center w-full">
+            <button
+                onClick={fileUpload}
+                className="bg-blue-500 rounded text-white font-bold py-2 px-4 w-full"
+            >
+                ファイルを選択
+            </button>
+            <input
+                type="file"
+                className="hidden"
+                accept=".jpg,.jpeg,.png"
+                ref={inputRef}
+                onChange={onFileInputChange}
+            />
+        </div>
+    )
+}
