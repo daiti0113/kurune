b611a742859fe041a9ad1700e755339dca11cf5c
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-21 01:44:12 +0900

    PrimaryButtonのスタイルを変更

--- components/atoms/Button/PrimaryButton.tsx
+++ components/atoms/Button/PrimaryButton.tsx
@@ -7,7 +7,7 @@ type PrimaryButtonProps = ButtonCoreProps
 export const PrimaryButton: React.FC<PrimaryButtonProps> = ({ children, className, isLoading, ...props }) => {
     return (
         <ButtonCore
-            className={classNames("bg-primary-500", className, isLoading && "bg-neutral-300 cursor-default")}
+            className={classNames("inline-block bg-primary-500 hover:bg-primary-700 active:bg-black-700 focus-visible:ring ring-black-300 text-white text-sm md:text-base font-semibold text-center rounded outline-none transition duration-100 px-8 py-2", className, isLoading && "bg-neutral-300 cursor-default")}
             isLoading={isLoading}
             {...props}
         >
