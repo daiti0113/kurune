430bfe492a2cd86ab5b9ca07553e4e93bf369ffa
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-23 00:39:33 +0900

    商品詳細画面のデザインを変更(WIP)

--- components/Article/index.tsx
+++ components/Article/index.tsx
@@ -11,49 +11,31 @@ type Props = {
 export default function Article({ data }: Props) {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>{data.title}</h1>
-      <TagList tags={data.tags} />
-      <p className={styles.description}>{data.description}</p>
-      <div className={styles.meta}>
-        {data.seller && (
-          <div className={styles.seller}>
-            <picture>
-              <source
-                type="image/webp"
-                srcSet={`${data.seller?.image?.url}?fm=webp&fit=crop&w=48&h=48 1x, ${data.seller?.image?.url}?fm=webp&fit=crop&w=48&h=48&dpr=2 2x`}
-              />
-              <img
-                src={data.seller?.image?.url}
-                alt=""
-                className={styles.sellerIcon}
-                width={data.seller?.image?.width}
-                height={data.seller?.image?.height}
-              />
-            </picture>
-            <span className={styles.sellerName}>{data.seller?.name}</span>
-          </div>
-        )}
-        <PublishedDate date={data.publishedAt || data.createdAt} />
+      <div className="flex gap-10 w-full">
+        <picture className="flex-[1.5_1_0%] max-w-xl">
+          <source
+            type="image/webp"
+            media="(max-width: 640px)"
+            srcSet={`${data.image}?fm=webp&w=414 1x, ${data.image}?fm=webp&w=414&dpr=2 2x`}
+          />
+          <source
+            type="image/webp"
+            srcSet={`${data.image}?fm=webp&fit=crop&w=960&h=504 1x, ${data.image}?fm=webp&fit=crop&w=960&h=504&dpr=2 2x`}
+          />
+          <img
+            src={data.image}
+            alt=""
+            className={styles.thumbnail}
+          />
+        </picture>
+        <div className="flex-1 w-full">
+          <h1 className={styles.title}>{data.title}</h1>
+          <TagList tags={data.tags} />
+          <p className={styles.description}>{data.description}</p>
+          <PublishedDate date={data.publishedAt || data.createdAt} />
+          <CommentForm article={data} />
+        </div>
       </div>
-      <picture>
-        <source
-          type="image/webp"
-          media="(max-width: 640px)"
-          srcSet={`${data.image}?fm=webp&w=414 1x, ${data.image}?fm=webp&w=414&dpr=2 2x`}
-        />
-        <source
-          type="image/webp"
-          srcSet={`${data.image}?fm=webp&fit=crop&w=960&h=504 1x, ${data.image}?fm=webp&fit=crop&w=960&h=504&dpr=2 2x`}
-        />
-        <img
-          src={data.image}
-          alt=""
-          className={styles.thumbnail}
-          width={data.thumbnail?.width}
-          height={data.thumbnail?.height}
-        />
-      </picture>
-      <CommentForm article={data} />
     </main>
   );
 }
