79260be9947161ca96eb8e4e85d8ed48f7c022f3
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-22 22:26:07 +0900

    バリデーションロジックをReact Hook Formに移行

--- components/organisms/RegisterForm/index.tsx
+++ components/organisms/RegisterForm/index.tsx
@@ -2,11 +2,9 @@
 
 import { Button } from "@/components/atoms/Button";
 import { ImageInput } from "@/components/atoms/ImageInput";
-import { Label } from "@/components/atoms/Label";
 import { Loading } from "@/components/atoms/Loading";
 import { Select } from "@/components/atoms/Select";
 import { TextArea } from "@/components/atoms/TextArea";
-import { TextInputCore } from "@/components/atoms/TextInputCore";
 import { InputContainer } from "@/components/molecules/InputContainer";
 import { TextInput } from "@/components/molecules/TextInput";
 import { useUpload } from "@/hooks/s3";
@@ -14,7 +12,7 @@ import { PostItemPayload, Tag } from "@/libs/microcms";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { RegisterOptions, useForm } from "react-hook-form";
 
 type FormData = Omit<PostItemPayload, "image"> & {
     image: FileList
@@ -24,8 +22,37 @@ type RegisterProps = {
     tags: Tag[]
 }
 
-
-// TODO: バリデーションルールを追加
+const options: {[key in keyof FormData]?: RegisterOptions<FormData, key>} = {
+    image: {
+        required: "画像を選択してください",
+    },
+    title: {
+        required: "タイトルを入力してください"
+    },
+    price: {
+       required: "価格を入力してください",
+       valueAsNumber: true
+    },
+    tags:{
+       required: "カテゴリを選択してください",
+    },
+    description: {
+       required: "説明を入力してください",
+    },
+    name: {
+       required: "名前を入力してください",
+    },
+    email: {
+       required: "メールアドレスを入力してください",
+       pattern: {
+        value: /\S+@\S+\.\S+/,
+        message: "メールアドレスの形式が誤っています",
+      },
+    },
+    tel: {
+       required: "電話番号を入力してください",
+    },
+}
 
 export const RegisterForm = ({ tags }: RegisterProps) => {
     const router = useRouter()
@@ -54,25 +81,27 @@ export const RegisterForm = ({ tags }: RegisterProps) => {
                 <div className="mt-10">
                     <h2 className="text-lg font-bold">商品情報</h2>
                     <div className="mt-4 flex flex-col gap-10">
-                        <TextInput label="商品画像" errorMessage="画像を添付してください" {...register("image")} />
-                        <TextInput label="タイトル" errorMessage="タイトルを入力してください" {...register("title")} />
-                        <TextInput label="価格" errorMessage="価格を入力してください" type="number" {...register("price", {valueAsNumber: true})} />
-                        <InputContainer label="カテゴリ（複数選択可）" errorMessage="カテゴリを選択してください">
-                            <Select required multiple {...register("tags")} >
+                        <InputContainer label="商品画像" errorMessage={errors.image?.message}>
+                            <ImageInput {...register("image", options.image)} />
+                        </InputContainer>
+                        <TextInput label="タイトル" errorMessage={errors.title?.message} {...register("title", options.title)} />
+                        <TextInput label="価格" errorMessage={errors.price?.message} type="number" {...register("price", options.price)} />
+                        <InputContainer label="カテゴリ（複数選択可）" errorMessage={errors.tags?.message}>
+                            <Select multiple {...register("tags", options.tags)} >
                                 {tags.map((tag) => <option key={tag.id} label={tag.name} value={tag.id} />)}
                             </Select>
                         </InputContainer>
-                        <InputContainer label="説明" errorMessage="説明を入力してください">
-                            <TextArea required {...register("description")} />
+                        <InputContainer label="説明" errorMessage={errors.description?.message}>
+                            <TextArea {...register("description", options.description)} />
                         </InputContainer>
                     </div>
                 </div>
                 <div className="mt-20">
                     <h2 className="text-lg font-bold">出品者情報</h2>
                     <div className="mt-4 flex flex-col gap-10">
-                        <TextInput label="名前" errorMessage="名前を入力してください" {...register("name")} />
-                        <TextInput label="メールアドレス" errorMessage="メールアドレスを入力してください" type="email" {...register("email")} />
-                        <TextInput label="電話番号" errorMessage="電話番号を入力してください" type="tel" {...register("tel")} />
+                        <TextInput label="名前" errorMessage={errors.name?.message} {...register("name", options.name)} />
+                        <TextInput label="メールアドレス" errorMessage={errors.email?.message} type="email" {...register("email", options.email)} />
+                        <TextInput label="電話番号" errorMessage={errors.tel?.message} type="tel" {...register("tel", options.tel)} />
                     </div>
                 </div>
                 <Button type="submit">出品する</Button>
