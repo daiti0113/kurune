94dc9b4b434b9978b6b712a1523eca9966977cfd
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-23 20:42:56 +0900

    人気カテゴリのスタイル修正

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export default function Nav({ categories }: Props) {
   return (
-    <div className="px-6 pb-6 mb-8 border-b-[1px]">
-      <h1 className="text-md font-bold">人気カテゴリ</h1>
+    <div className="pt-4 px-6 pb-6 mb-8 border-b-[1px]">
+      <h1 className="text-md font-bold sm:text-center">人気カテゴリ</h1>
       <nav className="flex flex-col items-center justify-center gap-2">
         <CategoryList categories={categories} />
       </nav>
