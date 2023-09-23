627a888d8a111a64079e7d8e43c3b2b9c6622aad
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-23 18:12:20 +0900

    人気カテゴリのラベルを追加

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -8,8 +8,11 @@ type Props = {
 
 export default function Nav({ categories }: Props) {
   return (
-    <nav className="flex flex-col items-center justify-center gap-2 px-6 pb-6 mb-8 border-b-[1px]">
-      <CategoryList categories={categories} />
-    </nav>
+    <div className="px-6 pb-6 mb-8 border-b-[1px]">
+      <h1 className="text-md font-bold">人気カテゴリ</h1>
+      <nav className="flex flex-col items-center justify-center gap-2">
+        <CategoryList categories={categories} />
+      </nav>
+    </div>
   );
 }
