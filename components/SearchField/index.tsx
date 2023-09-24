5a9997b93e785e561ac3f295ba0bb16dc596393d
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-24 21:50:02 +0900

    SearchFieldをボタンでもSubmitできるよう改善

--- components/SearchField/index.tsx
+++ components/SearchField/index.tsx
@@ -4,36 +4,40 @@ import { useCallback, useRef, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 
-// TODO: 検索実行ボタンを付ける
 export default function SearchField() {
   const [composing, setComposition] = useState(false);
+  const [keyword, setKeyword] = useState<string>()
   const startComposition = () => setComposition(true);
   const endComposition = () => setComposition(false);
   const _onEnter: React.KeyboardEventHandler<HTMLInputElement> = useCallback(
     (e) => {
       if (e.code === 'Enter' && !composing) {
-        location.href = `/search?q=${inputRef.current?.value}`;
+        location.href = `/search?q=${keyword}`;
       }
     },
-    [composing],
+    [composing, keyword],
   );
+  const onClick = () => {
+    location.href = `/search?q=${keyword}`;
+  }
   const inputRef = useRef<HTMLInputElement>(null);
   const searchParams = useSearchParams();
   const defaultQuery = searchParams.get('q') || '';
   return (
-    <label className="flex pr-6 pl-4 border-2 rounded-3xl border-black max-w-xl w-full h-10 items-center">
-      <div className="bg-[url('/search.svg')] bg-no-repeat h-4 w-4" />
+    <label className="flex pr-1 pl-4 border-2 rounded-full border-black max-w-xl w-full h-10 items-center">
       <input
         type="search"
         name="q"
         ref={inputRef}
-        className="pl-2 focus:outline-none"
+        className="w-full pl-2 focus:outline-none"
         placeholder="Search..."
         onKeyDown={_onEnter}
         onCompositionStart={startComposition}
         onCompositionEnd={endComposition}
         defaultValue={defaultQuery}
+        onChange={(e) => setKeyword(e.currentTarget.value)}
       />
+      <div className="bg-[url('/search.svg')] bg-no-repeat h-4 w-4 bg-primary-500 p-4 bg-center rounded-full" onClick={onClick} />
     </label>
   );
 }
