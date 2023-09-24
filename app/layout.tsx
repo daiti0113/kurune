0354a53a4b25c932b277b9653a7ff28a9571e9d0
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-24 22:47:52 +0900

    PWA対応

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,15 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Nav from '@/components/Nav';
 import './globals.css';
-import { Button } from '@/components/atoms/Button';
-import Link from 'next/link';
 import { Suspense } from 'react';
 import { Loading } from '@/components/atoms/Loading';
 import { Providers } from './Providers';
+import Head from 'next/head';
 
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
   title: 'kurune | 沖縄のフリマサイト',
-  description: '沖縄県限定の手渡し専用フリマサイトです。',
+  description: '【会員登録不要】手渡しのみの沖縄県限定フリマサイトです。',
   openGraph: {
     title: 'kurune | 沖縄のフリマサイト',
     description: '沖縄県限定の手渡し専用フリマサイトです。',
@@ -35,6 +34,11 @@ export default async function RootLayout({ children }: Props) {
   return (
     <Providers>
       <html lang="ja">
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
         <body className="h-screen flex flex-col">
           <Header />
           <Nav categories={categories.contents} />
