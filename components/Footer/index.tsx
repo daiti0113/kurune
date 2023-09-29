8649031c16da84587c890b8a68703387029e4077
daiti0113 <stennis.ds.0113@gmail.com>
2023-09-29 21:50:39 +0900

    Footerコンポーネントに利用規約とプラポリへのリンクを追加

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="py-4 px-6 text-center text-sm">
+    <footer className="flex flex-col py-4 px-6 justify-center items-center text-sm gap-3 sm:flex-row">
+      <Link className="text-primary-500" href={"/privacy"}>プライバシーポリシー</Link>
+      <Link className="text-primary-500" href={"/tos"}>利用規約</Link>
       <p>© Omochi. All Rights Reserved 2023</p>
     </footer>
   );
