ee17debf9ad21579fd5c5b8344c74a2981d1eb81
daiti0113 <stennis.ds.0113@gmail.com>
2023-10-05 20:47:40 +0900

    買い手にも問合せ完了通知メールを送信するよう変更

--- app/api/notify/email/route.ts
+++ app/api/notify/email/route.ts
@@ -2,17 +2,31 @@ import { Resend } from 'resend';
 import { NextRequest, NextResponse } from 'next/server';
 import { PostEmailNotifyPayload } from '@/libs/kuruneApi';
 
-const createContent = ({ article, comment }: PostEmailNotifyPayload) => {
-    const articleUrl = `${process.env.BASE_URL}/articles/${article.id}`
+const createContentForSeller = ({ article, comment }: PostEmailNotifyPayload) => {
     return (
         `<p><strong>※本メールは通知専用です。<br />こちらのメールには返信できませんのでご注意ください。</strong></p>
-        <p>以下の投稿について、お問い合わせがありました。</p>
-        <a href="${articleUrl}">${articleUrl}</a>
-        <p>下記のメールアドレスに直接ご連絡ください。</p>
+        <p>以下の商品について、お問い合わせがありました。</p>
+
+        <h2>商品情報</h2>
+        <p><strong>【タイトル】</strong>${article.title}</p>
+        <p><strong>【価格】</strong>${article.price.toLocaleString()}</p>
+
         <h2>お問い合わせ内容</h2>
+        <p>下記のメールアドレスに直接ご連絡ください。</p>
         <p><strong>【氏名】</strong>${comment.name}</p>
-        <p><strong>【メール】</strong>${comment.email}</p>
+        <p><strong>【メールアドレス】</strong>${comment.email}</p>
         <p><strong>【内容】</strong></p>
+        <p>${comment.comment}</p>`
+    )
+}
+
+const createContentForBuyer = ({ article, comment }: PostEmailNotifyPayload) => {
+    const articleUrl = `${process.env.BASE_URL}/articles/${article.id}`
+    return (
+        `<p>以下の投稿について、お問い合わせが完了しました。</p>
+        <a href="${articleUrl}">${articleUrl}</a>
+        <p>出品者からのご連絡をお待ち下さい。</p>
+        <h2>お問い合わせ内容</h2>
         <p>${comment.comment}</p>`
     )
 }
@@ -23,13 +37,20 @@ export async function POST(request: NextRequest) {
 
     try {
         const resend = new Resend(process.env.RESEND_API_KEY);
-        const res = await resend.emails.send({
+        const resForSeller = await resend.emails.send({
             from: 'kurune-notify@omochi-okinawa.com',
             to: body.article.email,
             subject: `【${body.article.title}に関するお問い合わせ】${body.comment.name}さんからコメントが来ています`,
-            html: createContent(body)
+            html: createContentForSeller(body)
         });
-        console.log({res})
+        console.log("売り手への確認メール:", { resForSeller })
+        const resForBuyer = await resend.emails.send({
+            from: 'kurune-notify@omochi-okinawa.com',
+            to: body.comment.email,
+            subject: `【kurune】お問い合わせが完了しました`,
+            html: createContentForBuyer(body)
+        });
+        console.log("買い手への確認メール:", { resForBuyer })
         return NextResponse.json({message: "success"})
     } catch (e) {
         console.error(e)
