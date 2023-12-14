2b59bcacda4638e8eb4d6ffa52c908841d00051c
daiti0113 <stennis.ds.0113@gmail.com>
2023-12-14 16:11:49 +0900

    デバッグ用のログ出力を追加

--- app/api/server-sitemap.xml/route.ts
+++ app/api/server-sitemap.xml/route.ts
@@ -5,13 +5,17 @@ export async function GET() {
   console.log("GET!!! /api/server-sitemap.xml")
   const baseURL = process.env.BASE_URL || ''
   console.log({baseURL})
+  const MICROCMS_SERVICE_DOMAIN = process.env.MICROCMS_SERVICE_DOMAIN
+  const MICROCMS_API_KEY = process.env.MICROCMS_API_KEY
+  
   let test: Array<any> = []
   try {
+    console.log({MICROCMS_SERVICE_DOMAIN, MICROCMS_API_KEY})
     const { contents } = await getList()
     test = contents
     console.log({contents})
   } catch (error) {
-    console.log({error})
+    console.error({error})
   }
   
   const lastmod = new Date().toISOString()
