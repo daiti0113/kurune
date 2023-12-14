e0f3ad27b44ba74b8da30ee4c23f143d44f08fbe
daiti0113 <stennis.ds.0113@gmail.com>
2023-12-14 14:28:24 +0900

    APIルートを利用して、動的なサイトマップを追加

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,10 @@
 module.exports = {
     siteUrl: process.env.BASE_URL || 'https://kurune.okinawa',
     generateRobotsTxt: true,
+    exclude: ['/server-sitemap.xml'],
+    robotsTxtOptions: {
+    additionalSitemaps: [
+        process.env.BASE_URL + "/api/server-sitemap.xml",
+    ],
+  }
 }
