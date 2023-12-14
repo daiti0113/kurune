d9a0ae856860d328dafb96e416802865000f2e25
daiti0113 <stennis.ds.0113@gmail.com>
2023-12-14 16:42:19 +0900

    不要なログ出力を削除

--- app/api/server-sitemap.xml/route.ts
+++ app/api/server-sitemap.xml/route.ts
@@ -2,24 +2,16 @@ import { client } from '@/libs/microcms'
 import { getServerSideSitemap } from 'next-sitemap'
 
 export async function GET() {
-  console.log("GET!!! /api/server-sitemap.xml")
-  const baseURL = process.env.BASE_URL || ''
-  console.log({baseURL})
+    const baseURL = process.env.BASE_URL || ''
+    const itemIds = await client.getAllContentIds({endpoint: 'items'})
+    const lastmod = new Date().toISOString()
 
-  const itemIds = await client.getAllContentIds({endpoint: 'items'})
-  console.log({itemIds})
+    const dynamicPaths = itemIds.map((id) => {
+        return {
+            loc: `${ baseURL }/articles/${id}`,
+            lastmod
+        }
+    })
 
-  const lastmod = new Date().toISOString()
-  console.log({lastmod})
-
-  const dynamicPaths = itemIds.map((id) => {
-    return {
-      loc: `${ baseURL }/articles/${id}`,
-      lastmod
-    }
-  })
-
-  console.log({dynamicPaths})
-
-  return getServerSideSitemap(dynamicPaths)
+    return getServerSideSitemap(dynamicPaths)
 }
