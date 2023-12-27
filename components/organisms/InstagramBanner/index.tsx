71c1107c21be8c99b1c7c2e177403111db362d96
daiti0113 <stennis.ds.0113@gmail.com>
2023-12-27 20:57:55 +0900

    aタグの中にaタグがあり、Next.jsのエラーが出ていたので修正

--- components/organisms/InstagramBanner/index.tsx
+++ components/organisms/InstagramBanner/index.tsx
@@ -12,10 +12,10 @@ export const InstagramBanner = () => {
             <div className="text-white py-8 md:py-12 px-4 w-full max-w-screen-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-md">
                 <div className="flex flex-col items-center w-full">
                     <p className="text-xl sm:text-2xl md:text-3xl mb-2 font-bold">kurune 公式インスタグラム</p>
-                    <p className="text-md sm:text-md md:text-xl mb-10 leading-none">新着商品を随時アップしています！📢</p>
-                    <Link href="https://www.instagram.com/kurune.okinawa/?hl=ja" target="_blank" className="bg-white text-black font-bold uppercase text-sm md:text-base rounded hover:bg-gray-200 hover:text-gray-800 transition duration-100 px-8 py-2">
+                    <p className="text-md sm:text-md md:text-xl mb-10 leading-none">{"新着商品を随時アップしています！📢\nフォローしてチェックしよう！"}</p>
+                    <span className="bg-white text-black font-bold uppercase text-sm md:text-base rounded hover:bg-gray-200 hover:text-gray-800 transition duration-100 px-8 py-2">
                         見てみる
-                    </Link>
+                    </span>
                 </div>
             </div>
         </Link>
