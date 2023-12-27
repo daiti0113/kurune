0701f1dc4ec89d8efa13c634db7643dbe5325e3c
daiti0113 <stennis.ds.0113@gmail.com>
2023-12-27 16:50:33 +0900

    中央寄せになっていなかったので修正

--- components/organisms/InstagramBanner/index.tsx
+++ components/organisms/InstagramBanner/index.tsx
@@ -8,8 +8,8 @@ export const InstagramBanner = () => {
     if (pathname !== "/") return null
 
     return (
-        <Link href="https://www.instagram.com/kurune.okinawa/?hl=ja" target="_blank" className="mx-4 md:mx-20">
-            <div className="text-white py-8 md:py-12 px-4 w-full max-w-screen-xl self-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-md">
+        <Link href="https://www.instagram.com/kurune.okinawa/?hl=ja" target="_blank" className="flex justify-center mx-4 md:mx-20">
+            <div className="text-white py-8 md:py-12 px-4 w-full max-w-screen-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-md">
                 <div className="flex flex-col items-center w-full">
                     <p className="text-xl sm:text-2xl md:text-3xl mb-2 font-bold">kurune 公式インスタグラム</p>
                     <p className="text-md sm:text-md md:text-xl mb-10 leading-none">新着商品を随時アップしています！📢</p>
